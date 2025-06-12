e1f327a054da9f3ecfe4a802541c09aae7ecea73
GabrielTorres25 <gabicjs87@gmail.com>
2025-06-12 00:47:00 -0300

    Trabajo Integrador Mate

---
+++ 08 Integrador Matematica - Programacion/torres.py
@@ -0,0 +1,65 @@
+# bucle for para pedir los dni
+dnis = [];
+
+for i in range(3):  # 3 integrantes. 
+    valido = False; 
+    while not valido:
+        dni = input(f"Ingresa el DNI del integrante {i+1}: ");
+        if not dni.isdigit():
+            print("Por favor ingresa solo numeros.");
+        elif len(dni) < 7 or len(dni) > 9:
+            print("El DNI debe tener entre 7 y 9 digitos.");
+        else:
+            dnis.append(dni);
+            valido = True;
+
+# bucle for para recorrer los dni
+for dni in dnis:
+    suma = 0
+    frecuencia = [0] * 10  ## creamos una lista vacia con 10, 0.
+    for digito in dni:      ## para registrar la frecuencia. 
+        frecuencia[int(digito)] += 1
+        suma += int(digito)
+
+    print("////////////////////////////")
+    print(f"DNI: {dni}")
+    print(f"Suma de los digitos: {suma}")
+    print("Frecuencia de cada digito:")
+    for i in range(10):
+        print(f"{i}: el numero aparece {frecuencia[i]} veces")
+        
+
+
+# conjuntos de digitos unicos por DNI
+conjuntos_digitos = [set(dni) for dni in dnis]
+
+# mostramos conjuntos de digitos
+def mostrar_digitos(conjuntos):
+    print("////////////////////////////")
+    print("Conjuntos de digitos unicos por DNI");
+    for i, conjunto in enumerate(conjuntos, 1):
+        print(f"DNI {i}: {sorted(conjunto)}"); # Mostrar ordenado
+    print("////////////////////////////")
+# funcion con operaciones entre conjuntos de digitos
+def operaciones_conjuntos(conjuntos):
+    c1, c2, c3 = conjuntos
+
+    union = c1 | c2 | c3
+    interseccion = c1 & c2 & c3
+    diferencia_1_2 = c1 - c2
+    diferencia_1_3 = c1 - c3
+    diferencia_2_3 = c2 - c3
+    diferencia_simetrica = c1 ^ c2 ^ c3
+## resultados 
+    print("////////////////////////////")
+    print("Operaciones entre conjuntos de dígitos")
+    print(f"Unión de todos: {sorted(union)}") #el sorted para que los digitos aparezcan ordenados de menor a mayor 
+    print(f"Intersección de todos: {sorted(interseccion)}") #facilitando su comparacion en este caso. 
+    print(f"Diferencia DNI 1 - DNI 2: {sorted(diferencia_1_2)}")
+    print(f"Diferencia DNI 1 - DNI 3: {sorted(diferencia_1_3)}")
+    print(f"Diferencia DNI 2 - DNI 3: {sorted(diferencia_2_3)}")
+    print(f"Diferencia simétrica entre todos: {sorted(diferencia_simetrica)}")
+
+# ejecutamos las funciones
+mostrar_digitos(conjuntos_digitos)
+operaciones_conjuntos(conjuntos_digitos)
